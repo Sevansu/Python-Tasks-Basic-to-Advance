dd44f4b7df0d5ac2d77f4113829185de54140543
Sevansu <44464850+Sevansu@users.noreply.github.com>
2020-04-26 13:26:47 +0530

    Add files via upload

---
+++ Task 2/task_2_02_A.py
@@ -0,0 +1,3 @@
+#2. Create a file 'task_2_02_A.py' and write the statement 'Print "I am in A"'. Now using another file 'task_2_02_B.py' Print this message on the screen [Do not define any function].
+
+var = "I am in A" 
---
+++ Task 2/task_2_02_B.py
@@ -0,0 +1,5 @@
+#2. Create a file 'task_2_02_A.py' and write the statement 'Print "I am in A"'. Now using another file 'task_2_02_B.py' Print this message on the screen [Do not define any function].
+
+import task_2_02_A
+b = str(task_2_02_A.var)
+print(b) 
